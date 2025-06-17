c6b935665188b602cac87e42d0e2b9be89f84e71
yeasinChowdhury <depressedman996@gmail.com>
2025-06-18 00:50:48 +0600

    add new change

--- src/Banner/BannerRight.jsx
+++ src/Banner/BannerRight.jsx
@@ -1,14 +1,11 @@
-
-
-
-import React from 'react';
+import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import img from "../assets/banner iphone slider imag.jpg";
-import { FaAngleLeft } from "react-icons/fa6";
-import { FaAngleRight } from "react-icons/fa6";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
+// Custom Next Arrow
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
   return (
@@ -26,12 +23,12 @@ function SampleNextArrow(props) {
       }}
       onClick={onClick}
     >
-      <FaAngleLeft color='black' size={20} /> 
+      <FaAngleRight color="white" size={20} />
     </div>
   );
 }
 
-
+// Custom Prev Arrow
 function SamplePrevArrow(props) {
   const { className, style, onClick } = props;
   return (
@@ -43,15 +40,13 @@ function SamplePrevArrow(props) {
         alignItems: "center",
         justifyContent: "center",
         background: "gray",
-        borderRadius: "0%",
+        borderRadius: "50%",
         left: "-25px",
-      
         zIndex: 2
       }}
       onClick={onClick}
     >
-      <FaAngleRight color='black' size={40} />
-
+      <FaAngleLeft color="white" size={20} />
     </div>
   );
 }
@@ -64,33 +59,54 @@ const BannerRight = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />
+    prevArrow: <SamplePrevArrow />,
+    appendDots: dots => (
+      <div
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "5px",
+          background: "rgba(0, 0, 0, 0.3)",
+          borderRadius: "10px"
+        }}
+      >
+        <ul style={{ margin: 0, padding: 0, display: "flex", gap: "10px" }}>{dots}</ul>
+      </div>
+    ),
+    customPaging: i => (
+      <div
+        style={{
+          width: "20px",
+          height: "20px",
+          borderRadius: "50%",
+          background: "white",
+          color: "black",
+          textAlign: "center",
+          lineHeight: "20px",
+          fontSize: "12px"
+        }}
+      >
+        {i + 1}
+      </div>
+    )
   };
 
   return (
-    <div className='w-[80%] ml-5'>
+    <div className="w-[80%] ml-5 relative">
       <Slider {...settings}>
-        <div>
-          <img src={img} className="w-full" alt="Slide 1" />
-          <h3>1</h3>
-        </div>
-        <div>
-          <img src={img} className="w-full" alt="Slide 2" />
-          <h3>2</h3>
-        </div>
-        <div>
-          <img src={img} className="w-full" alt="Slide 3" />
-          <h3>3</h3>
-        </div>
-        <div>
-          <img src={img} className="w-full" alt="Slide 4" />
-          <h3>4</h3>
-        </div>
+        {[1, 2, 3, 4].map((num) => (
+          <div key={num} className="relative">
+            <img src={img} className="w-full h-[400px] object-cover" alt={`Slide ${num}`} />
+            <h3 className="absolute top-4 left-4 text-white text-2xl font-bold">Slide {num}</h3>
+          </div>
+        ))}
       </Slider>
     </div>
   );
 };
 
 export default BannerRight;
-
-
