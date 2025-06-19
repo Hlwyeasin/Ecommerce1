ef36130d9129858b10041bf504235369fd2c07bc
yeasinChowdhury <depressedman996@gmail.com>
2025-06-20 00:19:14 +0600

    add new change

--- src/components/OurProduct/OurProduct.jsx
+++ src/components/OurProduct/OurProduct.jsx
@@ -1,11 +1,182 @@
-import React from 'react'
+import React from 'react';
+import Container from '../../Layout/Container';
+
+import { FaRegHeart, FaStarHalfStroke } from 'react-icons/fa6';
+import { LiaEyeSolid } from 'react-icons/lia';
+import image01 from "../../assets/exProduct.png";
+import image02 from "../../assets/exPro02.png";
+import image03 from "../../assets/exProi03.png";
+import image04 from "../../assets/expro04.png";
+
 
 const OurProduct = () => {
   return (
-    <div className='bg-red-200 py-5'>
-    
-    </div>
-  )
-}
+    <div className="pb-[122px]">
+      <Container>
 
-export default OurProduct
+
+        <div>
+          <div className=" flex items-center">
+            <div className="w-[20px] h-[40px] bg-primary rounded"></div>
+            <div>
+              <p className="font-primary text-primary font-semibold ml-[16px]">
+                Our Products
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="mt-5">
+          <div className="flex justify-between items-center">
+            <h3 className="font-secondary text-[36px] font-semibold text-black">
+              Explore Our Products
+            </h3>
+          </div>
+        </div>
+
+        {/* card part start */}
+        
+
+        <div className='flex justify-between'>
+
+              <div className=" relative w-[270px]">
+            <div className="absolute top-3 right-3">
+              <div className="w-[34px] h-[34px] rounded-full bg-white-400 flex justify-center items-center">
+                <FaRegHeart size={22} />
+              </div>
+              <div className=" mt-2 w-[34px] h-[34px] rounded-full bg-white-400 flex justify-center items-center">
+                <LiaEyeSolid size={22} />
+              </div>
+            </div>
+
+            <div className="bg-[#F5F5F5] py-[52px] px-[65px] rounded">
+              <img src={image01} alt="" />
+              
+            </div>
+
+            <div className="mt-4">
+              <p className=" font-primary font-medium">The north coat</p>
+              <p className="font-primary font-medium py-2 text-primary">
+                $260 <del className="text-[#7D8184]">$360</del>{' '}
+              </p>
+              <div className="flex items-center">
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <p className="  ml-2 font-primary font-medium text-[#7D8184]">
+                  (65)
+                </p>
+              </div>
+            </div>
+          </div>
+
+              <div className=" relative w-[270px]">
+            <div className="absolute top-3 right-3">
+              <div className="w-[34px] h-[34px] rounded-full bg-white-400 flex justify-center items-center">
+                <FaRegHeart size={22} />
+              </div>
+              <div className=" mt-2 w-[34px] h-[34px] rounded-full bg-white-400 flex justify-center items-center">
+                <LiaEyeSolid size={22} />
+              </div>
+            </div>
+
+            <div className="bg-[#F5F5F5] py-[52px] px-[65px] rounded">
+              {/* <img src={sellingimag} alt="" /> */}
+              <img src={image02} alt="" />
+              
+            </div>
+
+            <div className="mt-4">
+              <p className=" font-primary font-medium">The north coat</p>
+              <p className="font-primary font-medium py-2 text-primary">
+                $260 <del className="text-[#7D8184]">$360</del>{' '}
+              </p>
+              <div className="flex items-center">
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <p className="  ml-2 font-primary font-medium text-[#7D8184]">
+                  (65)
+                </p>
+              </div>
+            </div>
+          </div>
+              <div className=" relative w-[270px]">
+            <div className="absolute top-3 right-3">
+              <div className="w-[34px] h-[34px] rounded-full bg-white-400 flex justify-center items-center">
+                <FaRegHeart size={22} />
+              </div>
+              <div className=" mt-2 w-[34px] h-[34px] rounded-full bg-white-400 flex justify-center items-center">
+                <LiaEyeSolid size={22} />
+              </div>
+            </div>
+
+            <div className="bg-[#F5F5F5] py-[52px] px-[65px] rounded">
+            
+              <img src={image03} alt="" />
+              
+            </div>
+
+            <div className="mt-4">
+              <p className=" font-primary font-medium">The north coat</p>
+              <p className="font-primary font-medium py-2 text-primary">
+                $260 <del className="text-[#7D8184]">$360</del>{' '}
+              </p>
+              <div className="flex items-center">
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <p className="  ml-2 font-primary font-medium text-[#7D8184]">
+                  (65)
+                </p>
+              </div>
+            </div>
+          </div>
+              <div className=" relative w-[270px]">
+            <div className="absolute top-3 right-3">
+              <div className="w-[34px] h-[34px] rounded-full bg-white-400 flex justify-center items-center">
+                <FaRegHeart size={22} />
+              </div>
+              <div className=" mt-2 w-[34px] h-[34px] rounded-full bg-white-400 flex justify-center items-center">
+                <LiaEyeSolid size={22} />
+              </div>
+            </div>
+
+            <div className="bg-[#F5F5F5] py-[52px] px-[65px] rounded">
+               <img src={image04} alt="" />
+              
+            </div>
+
+            <div className="mt-4">
+              <p className=" font-primary font-medium">The north coat</p>
+              <p className="font-primary font-medium py-2 text-primary">
+                $260 <del className="text-[#7D8184]">$360</del>{' '}
+              </p>
+              <div className="flex items-center">
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <FaStarHalfStroke className="text-[#FFAD33]" />
+                <p className="  ml-2 font-primary font-medium text-[#7D8184]">
+                  (65)
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+         
+        
+      </Container>
+    </div>
+  );
+};
+
+export default OurProduct;
